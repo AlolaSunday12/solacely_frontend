675025d71b648e31eef3840c3b3ce71aadbccb92
AlolaSunday12 <alolasj@gmail.com>
2025-04-06 22:15:10 +0100

    App details

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,42 @@
-export default function App() {
+import React from 'react';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Profile from './pages/Profile';
+import Navbar from './components/Navbar';
+import axios from 'axios';
+import api from './api';
+import { useEffect, useState } from 'react';
+import { UserContext } from './context/userContext';
+
+function App() {
+  const [user, setUser] = useState(null); 
+
+  useEffect(() => {
+    const fetchProfile = async () => {
+      try {
+        const res = await api.get('/profile');
+        setUser(res.data.user);
+      } catch (err) {
+        setUser(null);
+      }
+    };
+
+    fetchProfile();
+  }, []);
+
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <UserContext.Provider value={{user, setUser}}>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+    </UserContext.Provider>
+  );
 }
+
+export default App;
