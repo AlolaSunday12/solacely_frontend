fb44f4ebfdfe023f748878e4ecf7bb8cd666874f
AlolaSunday12 <alolasj@gmail.com>
2025-04-15 15:00:16 +0100

    update on Api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000', // your backend
+  baseURL: 'http://localhost:5000', 
+  headers: {
+    'Content-Type': 'application/json',
+  },
   withCredentials: true,            // sends cookies with every request
 });
 
